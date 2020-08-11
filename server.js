61c913fd73839d72cc5ecc597b9860cbd9ccdf20
Michael Taylor <mikejtaylor22@gmail.com>
2020-08-11 11:11:14 -0400

    test

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
- app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 //  app.use(express.static(__dirname + '/dist/world-of-wordcraft'));
 
 app.get('/*', function(req,res) {
     
  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-//  res.sendFile(path.join(__dirname+'/dist/world-of-wordcraft/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/world-of-wordcraft/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
